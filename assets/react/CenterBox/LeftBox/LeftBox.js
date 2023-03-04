8a667ec05c18f72ebf1500342415cc07bd801266
SpaceSurfer1 <99202999+SpaceSurfer1@users.noreply.github.com>
2023-03-04 07:12:21 +0530

    changes

--- assets/react/CenterBox/LeftBox/LeftBox.js
+++ assets/react/CenterBox/LeftBox/LeftBox.js
@@ -1,14 +1,17 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './LeftBox.css';
 function LeftBox() {
     return(
         <div className="leftbox">
             <ul>
-                <li><a href="">Home</a></li>
+                {/* <li><a href="">Home</a></li>
                 <li><a href="">My Profile</a></li>
                 <li><a href="">Connections</a></li>
                 <li><a href="">Jobs</a></li>
-                <li><a href="">Messages</a></li>
+                <li><a href="">Messages</a></li> */}
+                <li><Link to='/'>Home</Link></li>
+                <li><Link to='myprofile'>My Profile</Link></li>
             </ul>
         </div>
     );
