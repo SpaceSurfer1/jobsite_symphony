5ccc7a1a7906f2b12829eeae1411f33298172f03
SpaceSurfer1 <99202999+SpaceSurfer1@users.noreply.github.com>
2023-03-09 00:24:10 +0530

    changes

--- assets/react/TopBox/TopBox.js
+++ assets/react/TopBox/TopBox.js
@@ -4,12 +4,17 @@ import SiteNameBox from './SiteNameBox';
 import './TopBox.css';
 import TopOptionsBox from './TopOptions';
 function TopBox() {
+    
+    const userRating = document.querySelector('#root');
+    var isAuthenticated = userRating.dataset.eventId;
+    console.log(isAuthenticated);
+    
     return(
         <div className="topbox">
             <SiteNameBox></SiteNameBox>
             <SearchBox></SearchBox>
             {/* <TopOptionsBox></TopOptionsBox> */}
-            <p>Logout</p>
+            <p>{isAuthenticated}</p>
         </div>
     );
     
