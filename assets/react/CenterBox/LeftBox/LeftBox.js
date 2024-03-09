2c1b840992d75721a45b25036d3ea8f945abd39e
SpaceSurfer1 <99202999+SpaceSurfer1@users.noreply.github.com>
2024-03-10 02:56:21 +0530

    change

--- assets/react/CenterBox/LeftBox/LeftBox.js
+++ assets/react/CenterBox/LeftBox/LeftBox.js
@@ -1,25 +1,28 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { FcHome } from "react-icons/fc";
+import { FaHome } from "react-icons/fa";
 import { TfiWrite } from "react-icons/tfi";
 import { FaUserFriends } from "react-icons/fa";
 import {CgProfile} from "react-icons/cg";
+import { GoGitMerge } from "react-icons/go";
+import { HiOutlineBriefcase } from "react-icons/hi";
 import './LeftBox.css';
 function LeftBox() {
     return(
         <div className="leftbox">
             <ul>
-                <li><span><FcHome/></span><Link to='dash'>Dashboard</Link></li>
+                <li><span><FaHome /></span><Link to='dash'>Dashboard</Link></li>
                 
                 <li><span><CgProfile/></span><Link to='myprofile'>My Profile</Link></li>
 
-                <li><span><TfiWrite/></span><Link to='myessays'>My Essays</Link></li>
+                <li><span><GoGitMerge /></span><Link to='myessays'>My Merged PRs</Link></li>
 
-                <li><span><TfiWrite/></span><Link to='write'>Write New</Link></li>
+                <li><span><TfiWrite/></span><Link to='write'>Write New Post</Link></li>
                 
-                <li><span><FaUserFriends/></span><Link to='topics'>Topics</Link></li>
+                <li><span><HiOutlineBriefcase/></span><Link to='topics'>Remote Jobs</Link></li>
                 
-                {/* <li><span><FaUserFriends/></span><Link to='connections'>Community</Link></li> */}
+                {/* <li><span><GoGitMerge /></span><Link to='connections'>Remote Internships</Link></li> */}
             </ul>
         </div>
     );
--- assets/react/CenterBox/MiddleBox/MiddleBoxMyEssays.js
+++ assets/react/CenterBox/MiddleBox/MiddleBoxMyEssays.js
@@ -1,60 +1,127 @@
+// import { StatusBar } from 'expo-status-bar';
+import React, { useState, useEffect } from 'react';
+// import { SafeAreaView, StyleSheet, Text, Image, View, ScrollView, ActivityIndicator, TextInput, Button } from 'react-native';
+import { Dots } from "react-activity";
 import axios from 'axios';
-import React, { useEffect, useState } from 'react';
+// import { Card } from 'react-native-elements';
 import './MiddleBoxMyEssays.css';
 function MiddleBoxMyEssays() {
 
-    const [myessay, setMyessay] = useState([]);
-    const [eff, setEff] = useState(false);
+    const handleSave = () => {
 
-    useEffect(() => {
-        axios.get(`/api/getmyessay`, { params: { offset: lastId } })
-        .then(function (response) {
-            if(response.data.length > 0)
-            {
-                console.log(response.data);
-                setMyessay((prevState) => [
-                    ...prevState,
-                    ...response.data,
-                  ]);
-                setLastId(response.data[response.data.length - 1].id);
+        const fetchRepoData = async () => {
+            try {
+              const response = await axios.get(
+                `https://api.github.com/repos/${owner}/${repo}/pulls/${pullNumber}`
+              );
+              if (response.status === 200) {
+                setRepoName(response.data.base.repo.full_name);
+              } else {
+                console.error(`Failed to fetch data: ${response.status}`);
+              }
+            } catch (error) {
+              console.error('Error fetching data:', error);
             }
-            else if(response.data.length == 0)
-            {
-                setVisible(false);
-            }
-            
-        })
-        .catch(function (error) {
-          console.log(error);
-        });
-    }, [eff]);
+          };
 
+        const fetchUserData = async (text) => {
+          try {
+            setError(null);
+            const response = await axios.get(`https://api.github.com/users/${text}`);
+            setUserData(response.data);
+            // console.log(response.data);
+          } catch (error) {
+            setError(error.message);
+          }
+        };
+    
+        const fetchPullRequests = async (text) => {
+          try {
+              const response = await axios.get('https://api.github.com/search/issues?q=is:pr+author:'+text+'');
+              setPullRequests(response.data.items);
+              console.log(response.data.items);
+              fetchRepoData();
+              setLoading(false);
+          } catch (error) {
+              console.error('Error fetching pull requests:', error);
+              setLoading(false);
+          }
+      };
+        // Implement your save logic here
+        console.log('Text input value:', text);
+        // You can use this value to save to a database, update state, etc.
+        fetchUserData(text);
+        fetchPullRequests(text);
+      };
+    
+    
+    
+    
+      const [pullRequests, setPullRequests] = useState([]);
+      const [loading, setLoading] = useState(true);
+      const [text, setText] = useState('');
+      const [userData, setUserData] = useState(null);
+      const [error, setError] = useState(null);
 
-    const [visible, setVisible] = useState(true);
-    const [lastId, setLastId] = useState(Number.MAX_SAFE_INTEGER);
-
-    const handleGetMore = () => {
-        setEff(!eff);
-    }
-
-
-    return(
-        <div className="middlebox-myessays">
-            {myessay.map((eachessay) => {
-                return(
-               <div className="postboxdiv" key={eachessay.id}>
-                <p>{eachessay.essay_title}</p>
-                <p>{eachessay.id +". "+ eachessay.essay}</p>
-               </div>);
-               
-        })}
-            
-            {visible && <button onClick={handleGetMore}>Show More</button>}
-            {!visible && <p>That's All</p>}
+      return (
+        <div>
+    
+    
+    {error ? (
+            <p>Error: {error}</p>
+          ) : userData ? (
+            <div>
+              <img
+                src={userData.avatar_url }
+                style={{ width: 100, height: 100, borderRadius: 50 }}
+              />
+              <p>{userData.name}</p>
+              <p>{userData.bio}</p>
+              {/* Render other user details as needed */}
+            </div>
+          ) : (
+            <p>Loading...</p>
+          )}
+          
+    
+    
+    
+    
+            {/* <Card containerStyle={styles.card}> */}
+              <input
                 
-            
+                
+                onChange={(event)=>{setText(event.target.value)}}
+                value={text}
+                
+              />
+              <button
+                title="Save"
+                onClick={() => handleSave()}
+              />
+            {/* </Card> */}
+    
+    
+            <p style={{ fontSize: 18, fontWeight: 'bold', marginBottom: 10 }}>GitHub Merged Pull Requests:</p>
+            {loading ? (
+                <Dots color='white' />
+            ) : (
+                <div>
+                    {pullRequests.map((pr,index) => (
+                        <div key={pr.id} style={{ fontSize: 14,marginBottom: 90,border: '1px solid green'  }}>
+                            <p>PR :</p>
+                            <p>{index+1}. {pr.title}</p>
+                            <p>Link :</p>
+                            <a href={pr.pull_request.html_url}><p>{pr.pull_request.html_url}</p></a>
+                            <p>Time :</p>
+                            <p>{pr.pull_request.merged_at}</p>
+                            {/* Render other PR details as needed */}
+                        </div>
+                    ))}
+                </div>
+            )}
         </div>
-    );
+      );
     
 }
 export default MiddleBoxMyEssays;
