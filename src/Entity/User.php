6d3ab9cf77140b356dc087de1949847021c23abb
Bhupesh Mishra <99202999+SpaceSurfer1@users.noreply.github.com>
2024-03-14 20:24:24 +0530

    changes

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -46,6 +46,12 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
      */
     private $path;
 
+    #[ORM\Column(length: 255)]
+    private ?string $firstName = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $lastName = null;
+
     public function __construct()
     {
         $this->userEssay = new ArrayCollection();
@@ -188,6 +194,30 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     public function setPath(string $path): self
     {
         $this->path = $path;
+
+        return $this;
+    }
+
+    public function getFirstName(): ?string
+    {
+        return $this->firstName;
+    }
+
+    public function setFirstName(string $firstName): self
+    {
+        $this->firstName = $firstName;
+
+        return $this;
+    }
+
+    public function getLastName(): ?string
+    {
+        return $this->lastName;
+    }
+
+    public function setLastName(string $lastName): self
+    {
+        $this->lastName = $lastName;
 
         return $this;
     }
